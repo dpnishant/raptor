fc014790dc2308401cfe9485abec348b0bba4489
Nishant Das Patnaik <nishant.dp@gmail.com>
2015-06-08 16:40:40 -0400

    Added reverse proxy support & confs

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 echo "gem: --no-document" >> ~/.gemrc
 apt-get update
 apt-get upgrade
-apt-get install -y git apache2 php5 php5-curl php5-common libapache2-mod-php5 php5-mcrypt libffi-dev ruby php5-cli cmake libssh-4 libssl-dev linux-headers-generic build-essential autoconf libtool pkg-config python-pip python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
+apt-get install -y git nginx apache2 php5 php5-curl php5-common libapache2-mod-php5 libapache2-mod-rpaf php5-mcrypt libffi-dev ruby php5-cli cmake libssh-4 libssl-dev linux-headers-generic build-essential autoconf libtool pkg-config python-pip python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 libgle3 python-dev python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
 pip install cffi
 wget https://codeload.github.com/libgit2/libgit2/tar.gz/v0.22.0 -O libgit2-0.22.0.tar.gz
 tar xzf libgit2-0.22.0.tar.gz
@@ -35,4 +35,13 @@ mkdir -p /var/raptor/scan_results
 chmod -R 777 /var/raptor/scan_results #development purpose only
 mkdir -p /var/www/html/raptor
 cp -r frontend/* /var/www/html/raptor
+rm -rf /etc/apache2/sites-available/raptor.conf
+rm -rf /etc/nginx/sites-available/raptor
+rm -rf /etc/apache2/sites-enabled/raptor.conf
+rm -rf /etc/nginx/sites-enabled/raptor
+cp -r confs/apache2/* /etc/apache2
+cp -r confs/nginx/* /etc/nginx
+ln -s /etc/apache2/sites-available/raptor.conf /etc/apache2/sites-enabled/raptor.conf
+ln -s /etc/nginx/sites-available/raptor /etc/nginx/sites-enabled/raptor
 service apache2 restart
+service nginx restart
